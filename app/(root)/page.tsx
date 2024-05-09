a13d678b3141472bdc51c8eff6fc553510174c74
arif noviarrizal <muhammad.noviarrizal@ist.id>
2024-05-10 00:20:38 +0700

    feat(bank card, right sidebar): adding bank card and right sidebar component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from '@/components/HeaderBox';
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react';
 
 const Home = () => {
-  const loggedIn = { firstName: 'Arif' };
+  const loggedIn = {
+    firstName: 'Arif',
+    lastName: 'Noviarrizal',
+    email: 'arifnoviarrizal@gmail.com',
+  };
 
   return (
     <section className="home">
@@ -23,6 +28,11 @@ const Home = () => {
           />
         </header>
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 1250.35 }, { currentBalance: 5000 }]}
+      />
     </section>
   );
 };
